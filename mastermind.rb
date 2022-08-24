5715d0b807038abb599b98c531a8b25fb0238cef
Potayto <p.o.tayto@outlook.com>
2022-08-23 18:34:19 -0700

    Refactor code
    - Removed unnecessary Codemaker class and replaced its functionality
      with Validate module.
    
    - Include Validate module in both PlayerCodebreaker and
      ComputerCodemaker classes.

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,10 @@
-class Codemaker
-  attr_reader :possible_colors
-
-  def initialize
-    @possible_colors = %w[red blue green purple yellow orange]
-  end
+module Validate
+  POSSIBLE_COLORS = %w[red blue green purple yellow orange].freeze
 end
 
-class ComputerCodemaker < Codemaker
+class ComputerCodemaker
+  include Validate
+
   attr_reader :hidden_code
 
   def initialize
@@ -15,29 +13,33 @@ class ComputerCodemaker < Codemaker
     # Randomly select an assortment of the possible colors to choose from to
     # create the code that is to be decoded by the player
     @hidden_code = Array.new(4).map do
-      possible_colors.sample
+      POSSIBLE_COLORS.sample
     end
   end
 end
 
 class PlayerCodebreaker
+  include Validate
+
   def initialize
     @player_guess = nil
   end
 
-  def take_player_input(possible_colors)
-    puts "The possible choices are #{possible_colors}"
-    puts 'Enter your guess (the name of the four colors, each separated by a space):'
-    @player_guess = gets.chomp.split
+  def player_guess
+    take_player_input
+    @player_guess
   end
 
-  def player_guess(possible_colors)
-    take_player_input(possible_colors)
-    @player_guess
+  private
+
+  def take_player_input
+    puts "The possible choices are #{POSSIBLE_COLORS}"
+    puts 'Enter your guess (the name of the four colors, each separated by a space):'
+    @player_guess = gets.chomp.split
   end
 end
 
 computer = ComputerCodemaker.new
-p computer.hidden_code
+puts "The code is #{computer.hidden_code}"
 player = PlayerCodebreaker.new
-p player.player_guess(computer.possible_colors)
+puts "Player guess is #{player.player_guess}"
