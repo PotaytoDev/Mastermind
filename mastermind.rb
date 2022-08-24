667b560493b5d342cc9ab098a77af320daf5ea82
Potayto <p.o.tayto@outlook.com>
2022-08-23 18:56:17 -0700

    Add logic to count the number of each color in the hidden code

--- mastermind.rb
+++ mastermind.rb
@@ -5,15 +5,23 @@ end
 class ComputerCodemaker
   include Validate
 
-  attr_reader :hidden_code
+  attr_reader :hidden_code, :colors_in_hidden_code
 
   def initialize
-    super
-
     # Randomly select an assortment of the possible colors to choose from to
     # create the code that is to be decoded by the player
     @hidden_code = Array.new(4).map do
       POSSIBLE_COLORS.sample
+    end
+    @colors_in_hidden_code = count_number_of_colors_in_hidden_code
+  end
+
+  private
+
+  def count_number_of_colors_in_hidden_code
+    hidden_code.reduce(Hash.new(0)) do |colors_hash, color|
+      colors_hash[color] += 1
+      colors_hash
     end
   end
 end
@@ -41,5 +49,9 @@ end
 
 computer = ComputerCodemaker.new
 puts "The code is #{computer.hidden_code}"
+puts "\n"
+puts "The number of each color in the code is #{computer.colors_in_hidden_code}"
+puts "\n"
 player = PlayerCodebreaker.new
 puts "Player guess is #{player.player_guess}"
+puts "\n"
