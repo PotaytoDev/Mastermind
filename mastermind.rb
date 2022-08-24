a7b843b31c1c582c1c72b32c7d1a0f289feb3123
Potayto <p.o.tayto@outlook.com>
2022-08-23 19:56:40 -0700

    Add logic to provide feedback based on player's guess

--- mastermind.rb
+++ mastermind.rb
@@ -47,11 +47,53 @@ class PlayerCodebreaker
   end
 end
 
-computer = ComputerCodemaker.new
-puts "The code is #{computer.hidden_code}"
-puts "\n"
-puts "The number of each color in the code is #{computer.colors_in_hidden_code}"
-puts "\n"
+class GameLogic
+  def compare_player_guess_with_code(player_guess_array, hidden_code, colors_left_to_match)
+    exact_matches = 0
+    correct_color_but_wrong_position = 0
+    exact_matches_indices = []
+
+    # Check to see how many of the colors in player's guess are in the exact
+    # position as those in the hidden code
+    4.times do |index|
+      color_guess = player_guess_array[index]
+      hidden_code_color = hidden_code[index]
+
+      if color_guess == hidden_code_color
+        exact_matches += 1
+        colors_left_to_match[color_guess] -= 1
+        exact_matches_indices.push(index)
+      end
+    end
+
+    indices_that_are_not_exact_match = (0..3).to_a - exact_matches_indices
+
+    # Check to see how many of the colors in player's guess are correct, but
+    # in the wrong position in comparison to those in hidden code
+    indices_that_are_not_exact_match.each do |index|
+      color_guess = player_guess_array[index]
+
+      if hidden_code.include?(color_guess) && colors_left_to_match[color_guess].positive?
+        colors_left_to_match[color_guess] -= 1
+        correct_color_but_wrong_position += 1
+      end
+    end
+
+    [exact_matches, correct_color_but_wrong_position]
+  end
+end
+
 player = PlayerCodebreaker.new
-puts "Player guess is #{player.player_guess}"
+computer = ComputerCodemaker.new
+code = computer.hidden_code
+colors_in_code = computer.colors_in_hidden_code
+player = PlayerCodebreaker.new
+player_guess = player.player_guess
+puts "The code is #{code}"
 puts "\n"
+puts "The number of each color in the code is #{colors_in_code}"
+puts "\n"
+puts "Player guess is #{player_guess}"
+puts "\n"
+game = GameLogic.new
+p game.compare_player_guess_with_code(player_guess, code, colors_in_code)
